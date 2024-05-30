efc30cf7bec5da86fe6b7350a3784b944e0732d2
Klemen03 <klemenkrklec10@gmail.com>
2024-05-30 20:27:15 +0200

    Initial commit

---
+++ components/providers/RootProviders.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+import React, { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+function RootProviders({ children }: { children: ReactNode }) {
+  const [queryClient] = React.useState(() => new QueryClient({}));
+
+  console.log('NODE_ENV:', process.env.NODE_ENV); // Add this line to check NODE_ENV
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default RootProviders;
